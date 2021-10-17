e50aeea8eb89cb3534341c62d44250fdc0e0ef15
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-17 21:33:45 +0100

    Develop dashboard logo

--- src/components/topBar/Topbar.jsx
+++ src/components/topBar/Topbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./topbar.css"
 
 export default function Topbar() {
     return (
         <div className="topbar">
         <div className="topbarWrapper">
-          <div className="topLeft">left</div>
+          <div className="topLeft">
+              <span className="logo">Dipo-Admin</span>
+          </div>
           <div className="topRight">right</div>  
         </div>
         </div>
