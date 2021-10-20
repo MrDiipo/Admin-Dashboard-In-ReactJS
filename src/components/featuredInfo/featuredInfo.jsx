e7262292cd467b96b483230215f92a10067eee8d
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-20 02:32:06 +0100

    FeaturedInfo component development

--- src/components/featuredInfo/featuredInfo.jsx
+++ src/components/featuredInfo/featuredInfo.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import "./featuredInfo.css"
+import {ArrowDownward, ArrowUpward} from "@mui/icons-material"
 
 function featuredInfo() {
     return (
         <div className="featured">
         <div className="featuredItem">
-            
-        </div>    
+            <span className="featuredTitle">Revenue</span>
+            <div className="featureMoneyContainer">
+                <span className="featuredMoney">#2,415</span>
+                <span className="featuredMoneyRate">-11.4 <ArrowDownward/></span>
+                
+            </div>
+            <span className="featuredSub">Compared to last month</span>
+        </div>  
+        <div className="featuredItem">
+            <span className="featuredTitle">Sales</span>
+            <div className="featureMoneyContainer">
+                <span className="featuredMoney">#4,415</span>
+                <span className="featuredMoneyRate">-1.4 <ArrowDownward/></span>
+                
+            </div>
+            <span className="featuredSub">Compared to last month</span>
+        </div>  
+        <div className="featuredItem">
+            <span className="featuredTitle">Cost</span>
+            <div className="featureMoneyContainer">
+                <span className="featuredMoney">#2,225</span>
+                <span className="featuredMoneyRate">+2.4 <ArrowUpward/></span>
+                
+            </div>
+            <span className="featuredSub">Compared to last month</span>
+        </div>  
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import "./home.css"
 
 function Home() {
     return (
         <div className="home">
-            Home
+            <FeaturedInfo/>
         </div>
     )
 }
